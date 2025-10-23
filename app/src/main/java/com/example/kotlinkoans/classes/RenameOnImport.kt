74cff977fc45f740a755886acb3f80ab00f148b6
KEN-4 <ssstututusss617@gmail.com>
2025-10-24 08:59:24 +0900

    #Classes  Rename on Importの解答

---
+++ app/src/main/java/com/example/kotlinkoans/classes/RenameOnImport.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinkoans.classes
+
+ import kotlin.random.Random as KRandom
+ import java.util.Random as JRandom
+
+fun useDifferentRandomClasses(): String {
+    return "Kotlin random: " +
+             KRandom.nextInt(2) +
+            " Java random:" +
+             JRandom().nextInt(2) +
+            "."
+}
+
+fun main() {
+    println(useDifferentRandomClasses())
+}
