487cf3dfa2948d909914b0a1616dc89671ad6529
KEN-4 <ssstututusss617@gmail.com>
2025-10-26 16:06:34 +0900

    #Conventions MyDateクラスを別ファイルに定義

---
+++ app/src/main/java/com/example/kotlinkoans/conventions/MyDate.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinkoans.conventions
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int  = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate) = DateRange(this, other)
