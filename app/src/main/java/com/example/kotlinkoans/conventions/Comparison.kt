519174776bc5b8ff74ab3b6305022b2635aea92a
KEN-4 <ssstututusss617@gmail.com>
2025-10-26 15:01:47 +0900

    #Conventions Comparisonの解答

---
+++ app/src/main/java/com/example/kotlinkoans/conventions/Comparison.kt
@@ -0,0 +1,19 @@
+package com.example.kotlinkoans.conventions
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int  = when {
+            year != other.year -> year - other.year
+            month != other.month -> month - other.month
+            else -> dayOfMonth - other.dayOfMonth
+    }
+}
+
+fun test(date1: MyDate, date2: MyDate) {
+    // this code should compile:
+    println(date1 < date2)
+}
+fun main() {
+    val a = MyDate(2000, 9, 5)
+    val b = MyDate(2000, 9, 10)
+    test(a, b)
+}
