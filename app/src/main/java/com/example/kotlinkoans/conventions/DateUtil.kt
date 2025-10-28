f90d1825ab7f1c3ecab4c8346acad7de4547586a
KEN-4 <ssstututusss617@gmail.com>
2025-10-29 08:53:41 +0900

    #Conventions addTimeIntervalsを追加

--- app/src/main/java/com/example/kotlinkoans/conventions/DateUtil.kt
+++ app/src/main/java/com/example/kotlinkoans/conventions/DateUtil.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinkoans.conventions
 
+import androidx.wear.watchface.complications.datasource.TimeInterval
 import java.util.Calendar
 
 /*
@@ -11,6 +12,21 @@ fun MyDate.followingDate(): MyDate {
     c.set(year, month, dayOfMonth)
     val millisecondsInADay = 24 * 60 * 60 * 1000L
     val timeInMillis = c.timeInMillis + millisecondsInADay
+    val result = Calendar.getInstance()
+    result.timeInMillis = timeInMillis
+    return MyDate(result.get(Calendar.YEAR), result.get(Calendar.MONTH), result.get(Calendar.DATE))
+}
+
+fun MyDate.addTimeIntervals(timeInterval: TimeInterval, amount: Int): MyDate {
+    val c = Calendar.getInstance()
+    c.set(year + if (timeInterval == TimeInterval.YEAR) amount else 0, month, dayOfMonth)
+    var timeInMillis = c.timeInMillis
+    val millisecondsInADay = 24 * 60 * 60 * 1000L
+    timeInMillis += amount * when (timeInterval) {
+        TimeInterval.DAY -> millisecondsInADay
+        TimeInterval.WEEK -> 7 * millisecondsInADay
+        TimeInterval.YEAR -> 0L
+    }
     val result = Calendar.getInstance()
     result.timeInMillis = timeInMillis
     return MyDate(result.get(Calendar.YEAR), result.get(Calendar.MONTH), result.get(Calendar.DATE))
